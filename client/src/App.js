b6a970b02b7b9226cb149e7e6882b4d3de518208
MichaelRGitLab <Michaelrenz97@web.de>
2021-03-03 13:33:20 +0100

    Introduced Header-Component with Styling

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Home from "./components/Home";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+      {/* Home_Banner */}
+      {/* Home_Cards */}
+      {/* Home_Footer */}
+    </div>
+  );
+}
+
+export default App;
